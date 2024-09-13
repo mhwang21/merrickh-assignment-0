8b19621ea2eec828fd739af6e306747ca196e788
Merrick <39192791+mhwang21@users.noreply.github.com>
2024-09-13 18:19:36 -0400

    Assignment 0

---
+++ add_two_numbers.py
@@ -0,0 +1,8 @@
+def add_two_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    number1 = 5
+    number2 = 10
+    result = add_two_numbers(number1, number2)
+    print(f"The sum of {number1} and {number2} is {result}")
